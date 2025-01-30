9b41b12fddc99c5303991dfc11f92c36307d9ec0
Tenorio <jvtv@ic.ufal.br>
2025-01-30 03:56:26 -0300

    Limpeza do código e rework na funcionalidade de Content Library Management
    Correção de bugs, novas funções para deixar o código mais limpo, comentários explicando as funções e uma reemplementação da função de Content Library Management, agora o catalogo de filmes e séries sera printado na tela, logo será implementado uma maneira de filtrar o conteúdo para cada gênero e/ou categoria.

--- Video-Streaming-Service.py
+++ Video-Streaming-Service.py
@@ -1,72 +1,89 @@
-user_quant = 0
+import os
+
 profile_list = []
 
-movie_catalog = [["filme_de_ação1", "filme_de_ação2", "filme_de_ação3"], ["filme_de_comedia1", "filme_de_comedia2", "filme_de_comedia3"], ["filme_de_romance1", "filme_de_romance2", "filme_de_romance3"]]
-show_catalog =  [["serie_de_ação1", "serie_de_ação2", "serie_de_ação3"], ["serie_de_comedia1", "serie_de_comedia2", "serie_de_comedia3"], ["serie_de_romance1", "serie_de_romance2", "serie_de_romance3"]]
+movie_catalog = {
+    "ação": ["filme de ação1", "filme de ação2", "filme de ação3"],
+    "comédia": ["filme de comedia1", "filme de comedia2", "filme de comedia3"],
+    "romance": ["filme de romance1", "filme de romance2", "filme de romance3"]
+}
 
+show_catalog = {
+    "ação": ["show de ação1", "show de ação2", "show de ação3"],
+    "comédia": ["show de comedia1", "show de comedia2", "show de comedia3"],
+    "romance": ["show de romance1", "show de romance2", "show de romance3"]
+}
+
+#Imprime todos os perfis criados no momento
+def print_profiles():
+    for i in range(len(profile_list)):
+        print(i, " - ", profile_list[i]["first"], profile_list[i]['last'])
+
+# Constroi um dicionario com as informações do perfil desejado do usuario
 def user_profile(first_name, last_name, user_age):
-    # Constroi um dicionario com as informações do perfil desejado do usuario
     user = {'first': first_name, 'last': last_name, 'age':user_age}
-
     return user
 
+#Pede as informações nescessárias do usuario para criar um perfil
 def create_profile():
-    #Pede as informações nescessárias para o usuario para criar um perfil
+    os.system('cls')
     print("Crie um perfil!")
     first_name = input("Primeiro nome: ")
     last_name = input("Sobrenome: ")
     age = input("Idade: ")
 
     profile = user_profile(first_name, last_name, age)
+    profile_list.append(profile)
     return profile
-    
-def catalog():
-    #Imprime o catalogo baseado na escolha do usuario
-    print("O que você pretende assistir, uma série ou um filme? (sem acentos e caracteres especiais))
-    
-    user_choice = input()
-    opt = 0
-    
-    if user_choice == "serie":
-        opt = 0
-    elif user_choice == "filme":
-        opt = 1
-    
-    print("Qual genero você que ver, ação ou comédia? (sem acentos e caracteres especiais)")
-    user_choice = input()
-    
-    if user_choice == "acao":
-        print(show_catalog[opt])
-    elif user_choice == "comedia":
-        print(movie_catalog[opt])
-    
-    
-    
 
-while(1):
-    if(user_quant == 0):
-        profile = create_profile()
-        profile_list.append(profile)
-        user_quant += 1
-        
-    else:
-        print("\nOlá, Gostaria de acessar algum perfil ou criar um novo?")
-        option = int(input("Acessar perfil - 1\nCriar um novo - 2\n"))
-        if option == 1:
-            for i in range(user_quant):
-                print(i, " - ", profile_list[i]["first"])
-                
-            user_choice = int(input("Qual perfil você gostaria de usar?"))
-            choosen_profile = profile_list[user_choice]
-            print("Olá ", choosen_profile["first"], choosen_profile["last"], "o que gostaria de assistir?")
-            catalog()
-            break
+#Exclui o perfil que o usuario pedir   
+def delete_profile():
+    print_profiles()
+    opt = int(input("Qual perfil você gostaria deletar?\n"))
+    assurance = str(input("Você tem certeza? Y/N\n"))
+
+    if assurance == "Y" or assurance == "y":
+        profile_list.pop(opt)
+
+#Escolhe o perfil
+def choose_profile():
+    print("Qual perfil você gostaria de usar?")
+    print_profiles()
+    user_choice = int(input())
+
+    global choosen_profile #Criando uma variavel global
+    choosen_profile = profile_list[user_choice]
+
+# Menu inicial de navegação
+def initial_menu():
+    while(1):
+        if len(profile_list) == 0: #Inicio do programa, nenhum perfil criado
+            create_profile()
+
         else:
-            profile = create_profile()
-            profile_list.append(profile)
-            user_quant += 1
+            os.system('cls') #Limpa a tela
+            print("Olá, Gostaria de acessar algum perfil ou criar um novo?")
+            option = int(input("Acessar perfil - 1\nCriar um novo - 2\nExcluir um perfil - 3\nSair - 4\n"))
 
+            if option == 1:
+                choose_profile()
+                main_menu()
+            elif option == 2:
+                create_profile()
+            elif option == 3:
+                delete_profile()
+            elif option == 4:
+                print("Deseja sair? Y/N")
+                assurance = str(input())
+                if assurance == "Y":
+                    break
 
-        
-
-
+# Menu inicial onde será mostrado o catálogo de filmes e séries
+def main_menu():
+    os.system('cls')
+    print("Olá", choosen_profile["first"], ", o que você gostaria de assistir?")
+    print(movie_catalog)
+    print(show_catalog)
+    input()
+    
+initial_menu()
